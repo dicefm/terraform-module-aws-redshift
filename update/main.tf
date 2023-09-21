7114cd95f60c3f7aa58ad543bb80a287b7350116
omar.shibli <omar.shibli@dice.fm>
2023-09-21 11:00:41 +0100

    redshift update module

---
+++ update/main.tf
@@ -0,0 +1,102 @@
+locals {
+  # if passed a value for redshift_subnet_group_name, we'll use that instead of creating a subnet group
+  redshift_subnet_group_name = coalesce(var.redshift_subnet_group_name, element(concat(aws_redshift_subnet_group.this.*.name, list("")), 0))
+
+  # if we were passed a value for parameter_group_name, we'll use that instead of creating a parameter group name
+  parameter_group_name = coalesce(var.parameter_group_name, element(concat(aws_redshift_parameter_group.this.*.id, list("")), 0))
+}
+
+provider "aws" {
+  region = local.region
+}
+
+resource "aws_redshift_cluster" "this" {
+  cluster_identifier = var.cluster_identifier
+  cluster_version    = var.cluster_version
+  node_type          = var.cluster_node_type
+  number_of_nodes    = var.cluster_number_of_nodes
+  cluster_type       = var.cluster_number_of_nodes > 1 ? "multi-node" : "single-node"
+  database_name      = var.cluster_database_name
+  master_username    = var.cluster_master_username
+  master_password    = var.cluster_master_password
+
+  port = var.cluster_port
+
+  vpc_security_group_ids = flatten(var.vpc_security_group_ids)
+
+  cluster_subnet_group_name    = local.redshift_subnet_group_name
+  cluster_parameter_group_name = local.parameter_group_name
+
+  publicly_accessible = var.publicly_accessible
+
+  # Snapshots and backups
+  final_snapshot_identifier           = var.final_snapshot_identifier != "false" ? var.final_snapshot_identifier : "0"
+  skip_final_snapshot                 = var.skip_final_snapshot
+  automated_snapshot_retention_period = var.automated_snapshot_retention_period
+  preferred_maintenance_window        = var.preferred_maintenance_window
+  allow_version_upgrade               = var.allow_version_upgrade
+
+  # IAM Roles
+  iam_roles = flatten(var.cluster_iam_roles)
+
+  # Encryption
+  encrypted  = var.encrypted
+  kms_key_id = var.kms_key_id
+
+  # Enhanced VPC routing
+  enhanced_vpc_routing = var.enhanced_vpc_routing
+
+  # Logging
+  logging {
+    enable        = var.enable_logging
+    bucket_name   = var.logging_bucket_name != "false" ? var.logging_bucket_name : ""
+    s3_key_prefix = var.logging_s3_key_prefix != "false" ? var.logging_s3_key_prefix : ""
+    # needed ? destination_region = 
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = ["master_password","database_name"]
+  }
+}
+
+resource "aws_redshift_parameter_group" "this" {
+  # if we were passed a value for parameter_group_name, don't bother creating a parameter group
+  count  = length(var.parameter_group_name) > 0 ? 0 : 1
+  name   = "${var.cluster_identifier}-${replace(var.cluster_parameter_group, ".", "-")}-custom-params"
+  family = var.cluster_parameter_group
+
+  parameter {
+    name  = "wlm_json_configuration"
+    value = var.wlm_json_configuration
+  }
+
+  parameter {
+    # ref: https://docs.aws.amazon.com/redshift/latest/mgmt/connecting-ssl-support.html
+    name  = "require_ssl"
+    value = var.require_ssl
+  }
+
+  parameter {
+    name  = "use_fips_ssl"
+    value = var.use_fips_ssl
+  }
+
+  parameter {
+    # ref: https://docs.aws.amazon.com/redshift/latest/mgmt/db-auditing.html
+    name  = "enable_user_activity_logging"
+    value = var.enable_user_activity_logging
+  }
+}
+
+resource "aws_redshift_subnet_group" "this" {
+  # if we passed a value for redshift_subnet_group_name, don't bother creating a subnet group
+  count = length(var.redshift_subnet_group_name) > 0 ? 0 : 1
+
+  name        = var.cluster_identifier
+  description = "Redshift subnet group of ${var.cluster_identifier}"
+  subnet_ids  = flatten(var.subnets)
+
+  tags = var.tags
+}
